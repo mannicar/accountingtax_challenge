1f6beee6a965da93c18fc3a445cd69140678de7d
Carlo Mannino <manninosca@gmail.com>
2019-08-21 16:13:44 -0400

    first commit

---
+++ tests.js
@@ -0,0 +1,15 @@
+function smallestInt(A) {
+  
+}
+
+function runTests() {
+  console.clear();
+  
+  // print results to console
+}
+
+function runBenchmark() {
+  console.clear();
+  
+  // print result to console
+}
