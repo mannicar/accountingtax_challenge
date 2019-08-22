871be2b55e511624c78884c4ae31e1952a245f87
Carlo Mannino <manninosca@gmail.com>
2019-08-21 22:38:16 -0400

    style

--- tests.js
+++ tests.js
@@ -1,3 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////
+// Pseudocode
+// function smallestInt(A):
+//   1. Generate array of possible integers
+//   2. Discard any potential negative integers
+//   3. Iterate over A to account for elements
+//   4. Iterate overordered sparse collection to find first gap greater than 0.
+///////////////////////////////////////////////////////////////////////////////
+// Benchmark results:
+// Calling smallestInt on the worst 
+//   case scenario with 100000 items 
+//   and a max value of 1000000 
+//   returned 1 ran in 99.4100000243634 ms
+///////////////////////////////////////////////////////////////////////////////
+// function smallestInt(A) {
+
+//   maxVal = Math.max(...A);
+
+//   if (maxVal <= 0) {
+//     return 1
+
+//   ordered = {}
+
+//   for (i = 0; i < maxVal; i++) {
+//     currVal = A[i];
+//     if (currVal < 0) {
+//       continue;
+//     }
+//     ordered[currVal] = currVal;
+//   }
+
+//   for (i = 1; i < maxVal; i++) {
+//     if (ordered[i] == null){
+//       return i
+//     }
+//   } 
+
+//   return maxVal+1
+// }
+///////////////////////////////////////////////////////////////////////////////
+// Benchmark Results:
+// Calling smallestInt on the worst 
+//   case scenario with 100000 items 
+//   and a max value of 1000000 
+//   returned 1 ran in 69.32999996934086 ms
+///////////////////////////////////////////////////////////////////////////////
 function smallestInt(A) {
 
   maxVal = Math.max(...A);
@@ -27,10 +73,12 @@ function smallestInt(A) {
 
   return maxVal+1
 }
-
+///////////////////////////////////////////////////////////////////////////////
 function runTests() {
   console.clear();
   console.log('Running tests...');
+
+  // print results to console
 
   // Test 1; should return 5
   arr1 = [1, 3, 6, 4, 1, 2]
@@ -65,7 +113,7 @@ function runTests() {
     // Test 4; should return 1,000,001
     arr4 = [...Array(100001).keys()]
     arr4.splice(0,1)
-    arr4 = worstCase.map((x) => x + 900001)
+    arr4 = arr4.map((x) => x + 900001)
     testVal4 = smallestInt(arr4);
     if (testVal4 == 1){
       console.log('Test 4 passed', testVal4);
@@ -77,16 +125,23 @@ function runTests() {
     arr5 = [...Array(100001).keys()]
     arr5.splice(0,1)
     testVal5 = smallestInt(arr5);
-    if (testMaxVal == 100001){
+    if (testVal5 == 100001){
       console.log('Test 5 passed', testVal5);
     } else {
       console.log('Test 5 failed', testVal5);
     }
 
-
-  // print results to console
 }
-
+///////////////////////////////////////////////////////////////////////////////
+// function benchmarkText(fun, inp) {
+//   var t0 = performance.now()
+//   res = fun(inp)
+//   var t1 = performance.now()
+//   return(`Function ${fun} was called with input ${inp}
+//   and returned a result ${res} with an execution time
+//   of ${t1-t0} ms`);
+// }
+///////////////////////////////////////////////////////////////////////////////
 function runBenchmark() {
   console.clear();
 
@@ -97,8 +152,11 @@ function runBenchmark() {
   worstRes = smallestInt(worstCase)
   var t1 = performance.now()
 
-  console.log(`Calling smallestInt on the worst case scenario with ${worstCase.length} items 
-  and a max value of ${Math.max(...worstCase)} returned ${worstRes} ran in ${t1-t0} ms`)
+  console.log(`Calling smallestInt on the worst 
+  case scenario with ${worstCase.length} items 
+  and a max value of ${Math.max(...worstCase)} 
+  returned ${worstRes} ran in ${t1-t0} ms`)
   
   // print result to console
 }
+///////////////////////////////////////////////////////////////////////////////
